1c63eab5d4909f000bfe1128a69e859892873f83
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-02 17:50:38 +0200

    Ok, until here just starting a new project to have completly clear what to do.. Until now just a new project created with individual account, ssh activated (it is working for some reason) and I just created a new controller, empty controller HelloWorldController where I wrote 2 functions to use the method GET in the URL

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return $"Hello {name}, NumTimes is: {numTimes}";
+        }
+
+    }
+}
