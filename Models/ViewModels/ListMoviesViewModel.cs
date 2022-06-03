699761785290dc9fef1ef03caf6c73b0d21fa690
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-03 23:47:07 +0200

    Reading data using DBContext, Lambda syntax, using View Model instead of ViewData and HTML Helpers to show a list with the title of the data: @Html.DisplayNameFor()

---
+++ Models/ViewModels/ListMoviesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models.ViewModels
+{
+    public class ListMoviesViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Ratings { get; set; }
+        public int ratingID { get; set; }
+
+    }
+}
