fedd347e8c70fd8d57f8bcf870a1c4a3f9090310
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-03 12:12:06 +0200

    Sending information from the controller to the View with the ViewData[] using the method GET too. Remember to use a function from the controller that returns a view, yoy have to create a view with the same name as the function. Love u. Bye

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -8,15 +8,20 @@ namespace MvcMovie.Controllers
 
         public IActionResult Index()
         {
+            
             return View();
         }
 
         // GET: /HelloWorld/Welcome
 
-        public string Welcome(string name, int numTimes = 1, int id = 1)
+        public IActionResult Welcome(string name, int numTimes = 1, int id = 1)
         {
             //HelloWorld/Welcome/2?name=David%20Silva&numtimes=3
-            return $"Hello {name}, NumTimes is: {numTimes} and the id: {id}";
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            //return $"Hello {name}, NumTimes is: {numTimes} and the id: {id}";
+            return View();
         }
 
     }
